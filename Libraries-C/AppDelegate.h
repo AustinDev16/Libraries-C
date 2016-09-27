37737bbcb379b6acee9228d11c9b3d4e8cc17019
AustinDev16 <austin.blaser@gmail.com>
2016-09-27 15:14:07 -0600

    Initial Commit

---
+++ Libraries-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Libraries-C
+//
+//  Created by Austin Blaser on 9/27/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Libraries-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Libraries-C
+//
+//  Created by Austin Blaser on 9/27/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
