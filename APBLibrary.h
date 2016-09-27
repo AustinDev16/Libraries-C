e0eacf2f391962cda668d3341bfcd7c255d27532
AustinDev16 <austin.blaser@gmail.com>
2016-09-27 15:30:19 -0600

    Add library object.

---
+++ APBLibrary.h
@@ -0,0 +1,23 @@
+//
+//  APBLibrary.h
+//  Libraries-C
+//
+//  Created by Austin Blaser on 9/27/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APBLibrary : NSObject
+
+-(instancetype)initWithName:(NSString *)name summary:(NSString *)summary language:(NSString *)language homepageURL:(NSURL *)homepageURL numberOfStars:(NSInteger)numberOfStars;
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *summary;
+@property (nonatomic, copy) NSString *language;
+@property (nonatomic, copy) NSURL *homepageURL;
+@property (nonatomic) NSInteger numberOfStars;
+
+@end
+
+
---
+++ APBLibrary.m
@@ -0,0 +1,26 @@
+//
+//  APBLibrary.m
+//  Libraries-C
+//
+//  Created by Austin Blaser on 9/27/16.
+//  Copyright © 2016 Austin Blaser. All rights reserved.
+//
+
+#import "APBLibrary.h"
+
+@implementation APBLibrary
+
+-(instancetype)initWithName:(NSString *)name summary:(NSString *)summary language:(NSString *)language homepageURL:(NSURL *)homepageURL numberOfStars:(NSInteger)numberOfStars
+{
+    self = [super init];
+    if (self){
+        _name = [name copy];
+        _summary = [summary copy];
+        _language = [language copy];
+        _homepageURL = [homepageURL copy];
+        _numberOfStars = numberOfStars;
+    }
+    return self;
+}
+
+@end
